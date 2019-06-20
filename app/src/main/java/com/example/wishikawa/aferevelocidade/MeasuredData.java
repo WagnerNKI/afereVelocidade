21b0cae342736e54c8727885df657c0cdbf35f7d
Wagner Ishikawa <wagnwnki@gmail.com>
2019-06-19 21:14:23 -0300

    Refactoring

---
+++ app/src/main/java/com/example/wishikawa/aferevelocidade/MeasuredData.java
@@ -0,0 +1,122 @@
+package com.example.wishikawa.aferevelocidade;
+
+public class MeasuredData {
+
+    private String plateLetterText;
+    private String plateNumberText;
+    private String velocityText;
+    private String colorText;
+    private String selectedBlock;
+    private String selectedFloor;
+    private String selectedResponsible;
+    private String offenderText;
+    private String beltText;
+    private String vehicleText;
+    private String plateComplete;
+    private String currentDate;
+    private String currentTime;
+
+    public String getPlateLetterText() {
+        return plateLetterText;
+    }
+
+    public void setPlateLetterText(String plateLetterText) {
+        this.plateLetterText = plateLetterText;
+    }
+
+    public String getPlateNumberText() {
+        return plateNumberText;
+    }
+
+    public void setPlateNumberText(String plateNumberText) {
+        this.plateNumberText = plateNumberText;
+    }
+
+    public String getVelocityText() {
+        return velocityText;
+    }
+
+    public void setVelocityText(String velocityText) {
+        this.velocityText = velocityText;
+    }
+
+    public String getColorText() {
+        return colorText;
+    }
+
+    public void setColorText(String colorText) {
+        this.colorText = colorText;
+    }
+
+    public String getSelectedBlock() {
+        return selectedBlock;
+    }
+
+    public void setSelectedBlock(String selectedBlock) {
+        this.selectedBlock = selectedBlock;
+    }
+
+    public String getSelectedFloor() {
+        return selectedFloor;
+    }
+
+    public void setSelectedFloor(String selectedFloor) {
+        this.selectedFloor = selectedFloor;
+    }
+
+    public String getSelectedResponsible() {
+        return selectedResponsible;
+    }
+
+    public void setSelectedResponsible(String selectedResponsible) {
+        this.selectedResponsible = selectedResponsible;
+    }
+
+    public String getOffenderText() {
+        return offenderText;
+    }
+
+    public void setOffenderText(String offenderText) {
+        this.offenderText = offenderText;
+    }
+
+    public String getBeltText() {
+        return beltText;
+    }
+
+    public void setBeltText(String beltText) {
+        this.beltText = beltText;
+    }
+
+    public String getVehicleText() {
+        return vehicleText;
+    }
+
+    public void setVehicleText(String vehicleText) {
+        this.vehicleText = vehicleText;
+    }
+
+    public String getPlateComplete() {
+        return plateComplete;
+    }
+
+    public void setPlateComplete(String plateComplete) {
+        this.plateComplete = plateComplete;
+    }
+
+    public String getCurrentDate() {
+        return currentDate;
+    }
+
+    public void setCurrentDate(String currentDate) {
+        this.currentDate = currentDate;
+    }
+
+    public String getCurrentTime() {
+        return currentTime;
+    }
+
+    public void setCurrentTime(String currentTime) {
+        this.currentTime = currentTime;
+    }
+}
